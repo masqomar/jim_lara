94bf0e9c08a9c468aa726f8ecd1f3645e0c57b09
masqomar <qomaruddin5@gmail.com>
2022-09-18 00:48:32 +0700

    edit

--- resources/views/admin/topup-anggota/create.blade.php
+++ resources/views/admin/topup-anggota/create.blade.php
@@ -14,43 +14,35 @@
             <div class="card">
                 <div class="card-body">
 
-                    <div class="row" style="background-color: lightgoldenrodyellow">
-                        <div class="alert alert-warning">
-                            <div class="media">
-                                <h6 class="mb-0 font-weight-normal">
-                                    Untuk mengurangi resiko topup, HARAP di input per 10 anggota saja!
-                                </h6>
-                            </div>
-                        </div>
-                        <table class="table table-bordered">
-                            <thead>
-                                <th>Nama Anggota</th>
-                                <th>Nominal</th>
-                                <th>Keterangan</th>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <select class="form-control" name="user_id">
-                                            <option value="" selected disabled>Pilih Anggota</option>
-                                            @foreach($users as $user)
-                                            <option value="{{$user->id}}">{{$user->nama}}</option>
-                                            @endforeach
-                                        </select>
-                                    </td>
-                                    <td><input type="number" name="nominal_topup" value="10000" class="form-control"></td>
-                                    <td><input type="text" name="keterangan" value="Voucher Bulanan" class="form-control" required></td>
+                    <table class="table table-bordered">
+                        <thead>
+                            <th>Nama Anggota</th>
+                            <th>Nominal</th>
+                            <th>Keterangan</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <select class="form-control" name="user_id">
+                                        <option value="" selected disabled>Pilih Anggota</option>
+                                        @foreach($users as $user)
+                                        <option value="{{$user->id}}">{{$user->nama}}</option>
+                                        @endforeach
+                                    </select>
+                                </td>
+                                <td><input type="number" name="nominal_topup" value="10000" class="form-control"></td>
+                                <td><input type="text" name="keterangan" value="Voucher Bulanan" class="form-control" required></td>
 
-                                </tr>
-                            </tbody>
-                        </table>
+                            </tr>
+                        </tbody>
+                    </table>
 
-                        <br>
+                    <br>
 
-                        <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                    <button type="submit" class="btn btn-primary pull-right">Submit</button>
 
-                    </div>
                 </div>
             </div>
         </div>
-        @stop
+    </div>
+    @stop
